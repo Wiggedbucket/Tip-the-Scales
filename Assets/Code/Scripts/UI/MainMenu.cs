cdc33713b9ed10e75247932b9b2d777ad13139ff
AutismoGismo <lucasvanhest2005@gmail.com>
2026-05-20 19:40:06 +0200

    Imported TMP and made a menu

---
+++ Assets/Code/Scripts/UI/MainMenu.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenuObject;
+    [SerializeField] GameObject settingsObject;
+    [SerializeField] GameObject creditsObject;
+
+    public void StartSingleplayer()
+    {
+
+    }
+
+    public void StartMultiplayer()
+    {
+
+    }
+
+    public void ToggleCredits()
+    {
+
+    }
+
+    public void ToggleSettings()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
