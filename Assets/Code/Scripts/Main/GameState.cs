b0fe36c8ade9d3c3117363ab0a846ce9739fc422
AutismoGismo <lucasvanhest2005@gmail.com>
2026-05-20 19:20:21 +0200

    Added GameState

--- Assets/Code/Scripts/Main/GameState.cs
+++ Assets/Code/Scripts/Main/GameState.cs
@@ -1,6 +1,41 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    
+    #region Singleton Setup
+    public static GameState Instance { get; private set; }
+    public static bool InstanceExists => Instance != null;
+
+    void Awake()
+    {
+        if (InstanceExists)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            // Registers the first valid instance before the rest of the scene startup flow.
+            Instance = this;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+    #endregion
+
+    public CombatPoints GlobalCombatPoints;
+    public List<CombatPoints> RoomCombatPointsList;
+}
+
+[System.Serializable]
+public struct CombatPoints
+{
+    public int angelPoints;
+    public int demonPoints;
 }
