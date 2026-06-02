d26d39f9470ea3b44885dde1f1c8001fb128ecae
Odstarva <white.glint2076@gmail.com>
2026-06-02 10:13:05 +0200

    Reworked Reader + Added Writing JSON and POST functionality

--- Assets/Code/Scripts/ServerConnector/UnityRequestFromServer.cs
+++ Assets/Code/Scripts/ServerConnector/UnityRequestFromServer.cs
@@ -1,30 +1,170 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System.Text;
+
+[System.Serializable]
+public class Objective
+{
+    public int light;
+    public int dark;
+}
 
 [System.Serializable]
 public class GameData
 {
-    public int LightScore;
-    public int DarkScore;
+    public float score;
+    public int objectives;
+
+    public Objective obj1;
+    public Objective obj2;
+    public Objective obj3;
 }
+
+[System.Serializable]
+public class ResetRequest
+{
+    public int objectives;
+}
+
 public class UnityRequestFromServer : MonoBehaviour
 {
+    private string url = "http://localhost:8080";
+    private string clientId = "unityClient1";
+
     IEnumerator Start()
     {
-        UnityWebRequest www = UnityWebRequest.Get("http://localhost:8080/getGameData");
-        yield return www.SendWebRequest();
-        if (www.result != UnityWebRequest.Result.Success)
+        yield return StartCoroutine(ResetGame());
+
+        yield return new WaitForSeconds(1);
+
+        GameData gameData = new GameData();
+
+        gameData.score = 0;
+        gameData.objectives = 3;
+
+        gameData.obj1 = new Objective();
+        gameData.obj2 = new Objective();
+        gameData.obj3 = new Objective();
+
+        gameData.obj1.light = 7;
+        gameData.obj1.dark = 0;
+
+        gameData.obj2.light = 7;
+        gameData.obj2.dark = 0;
+
+        gameData.obj3.light = 2;
+        gameData.obj3.dark = 1;
+
+        yield return StartCoroutine(
+            SendGameData(gameData)
+        );
+    }
+
+    IEnumerator ResetGame()
+    {
+        ResetRequest resetData =
+            new ResetRequest();
+
+        resetData.objectives = 3;
+
+        string json =
+            JsonUtility.ToJson(resetData);
+
+        byte[] bodyRaw =
+            Encoding.UTF8.GetBytes(json);
+
+        UnityWebRequest request =
+            new UnityWebRequest(
+                url + "/reset/" + clientId,
+                "POST"
+            );
+
+        request.uploadHandler =
+            new UploadHandlerRaw(bodyRaw);
+
+        request.downloadHandler =
+            new DownloadHandlerBuffer();
+
+        request.SetRequestHeader(
+            "Content-Type",
+            "application/json"
+        );
+
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
         {
-            Debug.Log(www.error);
+            Debug.LogError(
+                "Reset Failed: " +
+                request.error
+            );
         }
         else
         {
-            Debug.Log(www.downloadHandler.text);
-            string jsonResponse = www.downloadHandler.text;
-            GameData gameData = JsonUtility.FromJson<GameData>(jsonResponse);
-            Debug.Log("Light Score: " + gameData.LightScore);
-            Debug.Log("Dark Score: " + gameData.DarkScore);
+            Debug.Log(
+                "Reset Response: " +
+                request.downloadHandler.text
+            );
+        }
+    }
+
+    IEnumerator SendGameData(
+        GameData gameData
+    )
+    {
+        string json =
+            JsonUtility.ToJson(gameData);
+
+        Debug.Log(
+            "Sending: " + json
+        );
+
+        byte[] bodyRaw =
+            Encoding.UTF8.GetBytes(json);
+
+        UnityWebRequest request =
+            new UnityWebRequest(
+                url + "/" + clientId,
+                "POST"
+            );
+
+        request.uploadHandler =
+            new UploadHandlerRaw(bodyRaw);
+
+        request.downloadHandler =
+            new DownloadHandlerBuffer();
+
+        request.SetRequestHeader(
+            "Content-Type",
+            "application/json"
+        );
+
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(
+                "POST Failed: " +
+                request.error
+            );
+        }
+        else
+        {
+            Debug.Log(
+                "Server Response: " +
+                request.downloadHandler.text
+            );
+
+            GameData updatedData =
+                JsonUtility.FromJson<GameData>(
+                    request.downloadHandler.text
+                );
+
+            Debug.Log(
+                "New Score: " +
+                updatedData.score
+            );
         }
     }
 }
