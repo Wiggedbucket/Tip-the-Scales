d367ed1a047dee9059e466d79b777cd8dce57c5d
neYtork <justnetyou@gmail.com>
2026-06-03 17:46:06 +0200

    Sound Pooling
    Implemented sound pooling. It is customizable in the inspector.
    To make make a sound you need to use CreateSound of the SoundManager instance. you need to add SoundData (.WithSoundData(SoundData)), and you can add .WithPosition(vector3) and .WithRandomPitch().
    If the sound is used frequently (like a minigun shot) you can check frequentSound bool in SoundData.

--- Assets/Code/Scripts/Audio/WeaponSoundEvents.cs
+++ Assets/Code/Scripts/Audio/WeaponSoundEvents.cs
@@ -1,25 +1,33 @@
+using AudioSystem;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
-[Serializable]
-public class Sound
-{
-	public bool enabled = true;
-	public string name;
-	public AudioClip clip;
-}
+
 
 public class WeaponSoundEvents : MonoBehaviour
 {
-    public Sound[] sounds;
+	[Serializable]
+	public class WeaponSound
+	{
+		public string name;
+		public SoundData data;
+		public bool enabled;
+	}
+
+	public WeaponSound[] sounds;
 
 	public void PlaySound(string soundName)
 	{
-		Sound sound = Array.Find<Sound>(sounds, el => el.name == soundName);
+		WeaponSound sound = Array.Find<WeaponSound>(sounds, el => el.name == soundName);
 		if (sound.enabled)
-			SoundFXManager.instance.PLaySoundFXClip(sound.clip, transform, 1);
+		{
+			SoundManager.instance.CreateSound()
+			.WithSoundData(sound.data)
+			.Play();
+		}
+		
 	}
 }
--- Assets/Code/Scripts/Audio/test.cs
+++ Assets/Code/Scripts/Audio/test.cs
@@ -3,14 +3,28 @@ using UnityEngine.InputSystem;
 
 public class test : MonoBehaviour
 {
+    public Animator animator;
+    public float fireRate = .3f;
+    private float timer = 0;
     // Update is called once per frame
     void Update()
     {
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            MusicManager.instance.PlayMusicCrossfade("Room Music 1", 5f);
-
+            MusicManager.instance.PlayTrack("Room Music 1", 5f);
+            
         }
 
-    }
+
+        if (Mouse.current.leftButton.isPressed)
+        { 
+            if(timer >= fireRate)
+            {
+				animator.Play("Shoot", 0, 0f);
+                timer = 0;
+			}
+            
+        }
+		timer += Time.deltaTime;
+	}
 }
