5ecd5e137a70275e303f65bd611f287f9fc571f7
N <hoppennathan@gmail.com>
2026-06-10 11:41:32 +0200

    TempShootingBase
    Hitscan based shooting sort of works, needs lots of tweaks:
    Scriptable object weapons.
    Spread Shooting
    Hit Effects
    Proper Health Integration enemy type scripts, currently just a test script to destroy an object when health is done.
    
    Script has single fire mode and automatic fire mode. Need to assign things per weapon for shooting style.
    
    Conclusion: Currently just bare bones version that can somewhat be used for a demo

--- Assets/Code/Scripts/Player/PlayerShooting.cs
+++ Assets/Code/Scripts/Player/PlayerShooting.cs
@@ -1,27 +1,84 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+public enum fireMode {Automatic, SingleShot}
+
 public class PlayerShooting : MonoBehaviour
 {
-    public float Range = 100f;
-    public float Damage = 100f;
+    AudioSource audioSource;
+    Animator weaponAnimator;
 
+    [Header("WeaponStats")]
+    public float damage = 25f;
+    public float range = 100f;
+    public float firerate = 0.2f;
+    private float nextTimeToFire = 0f;
+
+    [Header("Firing Mode Settings")]
+    public fireMode currentFireMode = fireMode.Automatic;
+    private bool hasFiredSingle = false;
+
+    [Header("References")]
     public Camera playerCam;
+    private bool isShooting = false;
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        weaponAnimator = GetComponentInChildren<Animator>();
+    }
     void Update()
     {
-        
+        ShootCheck();
     }
 
     public void OnShoot(InputAction.CallbackContext context)
     {
-        if(context.performed)
+        if(context.started)
         {
-            RaycastHit hit;
-            if(Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit))
+            isShooting=true;
+        }
+        else if(context.canceled)
+        {
+            isShooting=false;
+            hasFiredSingle = false;
+        }
+    }
+
+    public void ShootCheck()
+    {
+        if (!isShooting || Time.time < nextTimeToFire) return;
+        {
+            if (currentFireMode == fireMode.Automatic )
             {
-                Debug.Log(hit.transform.name);
+                ExecuteShot();
+            }
+            else if (currentFireMode == fireMode.SingleShot)
+            {
+                if (!hasFiredSingle)
+                {
+                    ExecuteShot();
+                    hasFiredSingle=true;
+                }
+            }
+        }
+    }
+
+    public void ExecuteShot()
+    {
+        nextTimeToFire = Time.time + firerate;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.takeDamage(damage);
             }
         }
     }
