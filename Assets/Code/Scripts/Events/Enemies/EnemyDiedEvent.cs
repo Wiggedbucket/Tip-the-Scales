df94aadb2bcafdf26fce6bbea27a5371d9820708
Wiggedbucket <lucasvanhest2005@gmail.com>
2026-06-18 11:15:43 +0200

    Enemy died event points

--- Assets/Code/Scripts/Events/Enemies/EnemyDiedEvent.cs
+++ Assets/Code/Scripts/Events/Enemies/EnemyDiedEvent.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 public class EnemyDiedEvent : IEvent
 {
     public GameObject EnemyObject;
-    // Add enemy tier and other useful info later when the Enemy base class is done
+    public int Points;
 }
